c18dcedff1fe3751a9dbef7d3833b65b6674b4df
namtc07 <truongcongnam1207@gmail.com>
2025-02-18 12:08:10 +0700

    feat: setup home

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,201 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'dart:math';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const appTitle = 'Drawer Demo';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Root widget
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My Home Page'),
-        ),
-        body: Center(
-          child: Builder(
-            builder: (context) {
-              return Column(
-                children: [
-                  const Text('Hello, World!'),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      print('Click!');
-                    },
-                    child: const Text('A button'),
-                  ),
-                ],
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  final Random _random = Random();
+  final ScrollController _scrollController = ScrollController();
+  List<String> imageUrls = List.generate(
+      15,
+      (index) =>
+          'https://picsum.photos/seed/${Random().nextInt(1000)}/300/200');
+  Map<String, bool> imageLoadingStatus = {};
+
+  void _addImage() {
+    setState(() {
+      imageUrls
+          .add('https://picsum.photos/seed/${_random.nextInt(1000)}/300/200');
+    });
+  }
+
+  void _loadMoreImages() {
+    setState(() {
+      imageUrls.addAll(List.generate(
+          6,
+          (index) =>
+              'https://picsum.photos/seed/${_random.nextInt(1000)}/300/200'));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+          _scrollController.position.maxScrollExtent) {
+        _loadMoreImages();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  // Tạo một phương thức để xây dựng ảnh và hiển thị trạng thái loading
+  Widget buildImage(String imageUrl) {
+    // Kiểm tra trạng thái loading của ảnh
+    bool isLoading = imageLoadingStatus[imageUrl] ?? true;
+
+    return Stack(
+      children: [
+        Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) {
+              // Khi ảnh đã tải xong, đánh dấu là không còn loading nữa
+              imageLoadingStatus[imageUrl] = false;
+              return child;
+            } else {
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          (loadingProgress.expectedTotalBytes ?? 1)
+                      : null,
+                ),
               );
-            },
+            }
+          },
+        ),
+        if (isLoading)
+          Center(
+            child: Text(
+              'Loading...',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _addImage,
+          ),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Home'),
+            Tab(text: 'Category'),
+            Tab(text: 'Favorite'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: GridView.builder(
+              controller: _scrollController,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 1 / 2, // Hình chữ nhật
+              ),
+              itemCount: imageUrls.length,
+              itemBuilder: (context, index) {
+                return Image.network(imageUrls[index], fit: BoxFit.cover);
+              },
+            ),
+          ),
+          const Center(
+              child: Text('Category Section', style: TextStyle(fontSize: 20))),
+          const Center(
+              child: Text('Favorite Section', style: TextStyle(fontSize: 20))),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              onTap: () {
+                _tabController.animateTo(0);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Category'),
+              onTap: () {
+                _tabController.animateTo(1);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Favorite'),
+              onTap: () {
+                _tabController.animateTo(2);
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
     );
